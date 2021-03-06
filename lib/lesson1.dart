3b995d814ae0fc42255c41a5184d85b0034fd7a9
Daria Denisova <denisova.dasha1999@yandex.ru>
2021-03-06 19:46:02 +0300

    Выполненные задания 1 урока

--- lib/lesson1.dart
+++ lib/lesson1.dart
@@ -41,7 +41,7 @@ class Lesson1 {
 /// Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
 /// Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
 int seconds(int hours, int minutes, int seconds) {
-  //TODO
+  return hours * 3600 + minutes * 60 + seconds;
 }
 
 /// Тривиальная (1 балл)
@@ -50,7 +50,11 @@ int seconds(int hours, int minutes, int seconds) {
 /// Определить длину того же отрезка в метрах (в данном случае 18.98).
 /// 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
 double lengthInMeters(int sagenes, int arshins, int vershoks) {
-  //TODO
+  double vershRes = vershoks * 0.04445;
+  double arshRes = arshins * 0.7112;
+  double sagRes = sagenes * 2.1336;
+  double result = vershRes + arshRes + sagRes;
+  return num.parse(result.toStringAsFixed(2));
 }
 
 /// Простая (2 балла)
@@ -58,7 +62,9 @@ double lengthInMeters(int sagenes, int arshins, int vershoks) {
 /// Пользователь задает целое число, большее 100 (например, 3801).
 /// Определить третью цифру справа в этом числе (в данном случае 8).
 int thirdDigit(int number) {
-  //TODO
+  int left = number % 1000;
+  int numberRes = left ~/ 100;
+  return numberRes;
 }
 
 /// Простая (2 балла)
@@ -68,7 +74,7 @@ int thirdDigit(int number) {
 /// Определите время поезда в пути в минутах (в данном случае 216).
 int travelMinutes(
     int hoursDepart, int minutesDepart, int hoursArrive, int minutesArrive) {
-  //TODO
+  return hoursArrive * 60 + minutesArrive - (hoursDepart * 60 + minutesDepart);
 }
 
 /// Простая (2 балла)
@@ -77,7 +83,9 @@ int travelMinutes(
 /// Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
 /// Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
 double accountInThreeYears(int initial, int percent) {
-  //TODO
+  double p = percent / 100 + 1;
+  double tripleP = p * p * p;
+  return num.parse((initial * tripleP).toStringAsFixed(1));
 }
 
 /// Простая (2 балла)
@@ -85,7 +93,11 @@ double accountInThreeYears(int initial, int percent) {
 /// Пользователь задает целое трехзначное число (например, 478).
 /// Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
 int numberRevert(int number) {
-//TODO
+  int firstNum = number ~/ 100;
+  int secondNum = number % 100 ~/ 10;
+  int thirdNum = number % 10;
+
+  return thirdNum * 100 + secondNum * 10 + firstNum;
 }
 
 /// Простая (2 балла)
@@ -93,7 +105,16 @@ int numberRevert(int number) {
 /// Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
 /// Определить, счастливое ли заданное число, вернуть true, если это так.
 bool isNumberHappy(int number) {
-  //TODO
+  int firstNum = number ~/ 1000;
+  int secondNum = number % 1000 ~/ 100;
+  int thirdNum = number % 100 ~/ 10;
+  int fourthNum = number % 10;
+
+  if (firstNum + secondNum == thirdNum + fourthNum) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /// Простая (2 балла)
@@ -102,7 +123,11 @@ bool isNumberHappy(int number) {
 /// Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
 /// Считать, что ферзи не могут загораживать друг друга.
 bool queenThreatens(int x1, int y1, int x2, int y2) {
-  //TODO
+  if ((x1 == x2) || (y1 == y2) || (x1 - x2).abs() == (y1 - y2).abs()) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /// Простая (2 балла)
@@ -110,5 +135,22 @@ bool queenThreatens(int x1, int y1, int x2, int y2) {
 /// Дан номер месяца (от 1 до 12 включительно) и год (положительный).
 /// Вернуть число дней в этом месяце этого года по григорианскому календарю.
 int daysInMonth(int month, int year) {
-  //TODO
+  if (month == 2) {
+    final bool isLeapYear =
+        (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+    if (isLeapYear) return 29;
+    return 28;
+  }
+
+  if (month == 1 ||
+      month == 3 ||
+      month == 5 ||
+      month == 7 ||
+      month == 8 ||
+      month == 10 ||
+      month == 12) {
+    return 31;
+  } else {
+    return 30;
+  }
 }
