210aa5b8107749cdc0ab26900d0c7ef37083eb23
DatGuyLothirc <denissteam1@mail.ru>
2021-03-06 12:27:15 +0300

    Second lesson complete

--- lib/lesson2.dart
+++ lib/lesson2.dart
@@ -73,7 +73,10 @@ class Lesson2 {
 /// Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
 /// вернуть строку вида: «21 год», «32 года», «12 лет».
 String ageDescription(int age) {
-  //TODO
+  final special = age % 100 >= 11 && age % 100 <= 20;
+  if (age % 10 == 1 && !special) return '${age} год';
+  if (age % 10 >= 2 && age % 10 <= 4 && !special) return '${age} года';
+  return '${age} лет';
 }
 
 /// Простая (2 балла)
@@ -85,7 +88,13 @@ String ageDescription(int age) {
 /// и 3, если угроза есть и от ладьи и от слона.
 /// Считать, что ладья и слон не могут загораживать друг друга.
 int rookOrBishopThreatens(int kingX, kingY, rookX, rookY, bishopX, bishopY) {
-//TODO
+  final threatOnV = kingX == rookX;
+  final threatOnH = kingY == rookY;
+  final threatOnD = (kingX - bishopX).abs() == (kingY - bishopY).abs();
+  if (threatOnD && (threatOnH || threatOnV)) return 3;
+  if (threatOnD) return 2;
+  if (threatOnH || threatOnV) return 1;
+  return 0;
 }
 
 /// Простая (2 балла)
@@ -95,7 +104,22 @@ int rookOrBishopThreatens(int kingX, kingY, rookX, rookY, bishopX, bishopY) {
 /// прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
 /// Если такой треугольник не существует, вернуть -1.
 int triangleKind(double a, b, c) {
-//TODO
+  if (c > a + b || a > b + c || b > a + c) return -1;
+  if (c < a) {
+    final temp = c;
+    c = a;
+    a = temp;
+  }
+  if (c < b) {
+    final temp = c;
+    c = b;
+    b = temp;
+  }
+  final cSqr = c * c;
+  final sqrSumm = a * a + b * b;
+  if (cSqr < sqrSumm) return 0;
+  if (cSqr == sqrSumm) return 1;
+  return 2;
 }
 
 /// Простая (2 балла)
@@ -105,7 +129,12 @@ int triangleKind(double a, b, c) {
 ///
 /// Использовать операции со строками в этой задаче запрещается.
 int digitNumber(int n) {
-//TODO
+  var digitNum = 0;
+  do {
+    n = n ~/ 10;
+    digitNum++;
+  } while (n >= 1);
+  return digitNum;
 }
 
 /// Простая (2 балла)
@@ -113,7 +142,14 @@ int digitNumber(int n) {
 /// Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
 /// Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
 int fib(int n) {
-//TODO
+  switch(n) {
+    case 1:
+      return 1;
+    case 2:
+      return 1;
+    default:
+      return fib(n - 2) + fib(n - 1);
+  }
 }
 
 /// Средняя (3 балла)
@@ -121,7 +157,13 @@ int fib(int n) {
 /// Для заданных чисел m и n найти наименьшее общее кратное, то есть,
 /// минимальное число k, которое делится и на m и на n без остатка
 int lcm(int m, n) {
-//TODO
+  final mlp = m * n;
+  while (n != 0) {
+    final temp = m;
+    m = n;
+    n = temp % n;
+  }
+  return mlp ~/ m;
 }
 
 /// Средняя (3 балла)
@@ -130,7 +172,12 @@ int lcm(int m, n) {
 /// Взаимно простые числа не имеют общих делителей, кроме 1.
 /// Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
 bool isCoPrime(int m, n) {
-//TODO
+  while (n != 0) {
+    final temp = m;
+    m = n;
+    n = temp % n;
+  }
+  return m == 1;
 }
 
 /// Средняя (3 балла)
@@ -139,7 +186,13 @@ bool isCoPrime(int m, n) {
 ///
 /// Использовать операции со строками в этой задаче запрещается.
 int revert(int n) {
-//TODO
+  if (n < 10) return n;
+  var num = 0;
+  do {
+    num = num * 10 + n % 10;
+    n = n ~/ 10;
+  } while (n >= 1);
+  return num;
 }
 
 /// Средняя (3 балла)
@@ -150,7 +203,13 @@ int revert(int n) {
 ///
 /// Использовать операции со строками в этой задаче запрещается.
 bool isPalindrome(int n) {
-//TODO
+  while (n > 9) {
+    final a = n ~/ pow(10, digitNumber(n) - 1);
+    final b = n % 10;
+    if (a != b) return false;
+    n = n ~/ 10 % pow(10, digitNumber(n) - 2);
+  }
+  return true;
 }
 
 /// Сложная (4 балла)
@@ -161,7 +220,18 @@ bool isPalindrome(int n) {
 ///
 /// Использовать операции со строками в этой задаче запрещается.
 int squareSequenceDigit(int n) {
-//TODO
+  var i = 1;
+  var sqr;
+  while (n > 0) {
+    sqr = i * i;
+    n -= digitNumber(sqr);
+    i++;
+  }
+  while(n < 0) {
+    sqr = sqr ~/ 10;
+    n++;
+  }
+  return sqr % 10;
 }
 
 /// Сложная (5 баллов)
@@ -172,5 +242,16 @@ int squareSequenceDigit(int n) {
 ///
 /// Использовать операции со строками в этой задаче запрещается.
 int fibSequenceDigit(int n) {
-//TODO
+  var i = 1;
+  var fb;
+  while (n > 0) {
+    fb = fib(i);
+    n -= digitNumber(fb);
+    i++;
+  }
+  while(n < 0) {
+    fb = fb ~/ 10;
+    n++;
+  }
+  return fb % 10;
 }
